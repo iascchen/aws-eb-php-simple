54f5a4efe243a6a680786445178378b25c80804e
Hao Chen <chenhao@microduino.cc>
2020-04-28 21:23:54 +0800

    index with copy

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
   <title>PHP 测试</title>
  </head>
  <body>
- <h1>Hello AWS PHP</h1>
+ <h1>Hello AWS PHP!</h1>
  <?php echo '<p>Run PHP in Docker of Elastic Beanstalk</p>'; ?>
  <?php echo '<p>CHEN Hao for AWS Assignment</p>'; ?>
  <?php phpinfo(); ?>
